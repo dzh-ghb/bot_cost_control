677b0862f5511a91a328bb66e4c3fea2983f354f
dzh <d.snitchester@gmail.com>
2024-11-29 23:14:08 +0300

    add dummy web server to check application status

--- src/main/java/Success.java
+++ src/main/java/Success.java
@@ -2,11 +2,15 @@ import config.Config;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import server.DummyServer;
+
+import java.io.IOException;
 
 public class Success {
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException, IOException {
         Config.readData();
         System.out.println("I'll do it.\nNo doubts.");
+        DummyServer.startServer();
         TelegramBot telegramBot = new TelegramBot(); //создание инстанса бота
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class); //создание API
         telegramBotsApi.registerBot(telegramBot); //регистрация бота в API
---
+++ src/main/java/server/DummyServer.java
@@ -0,0 +1,27 @@
+package server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+//сервер, предназначенный для того, чтобы хостинг понимал, что приложение активно путем отправки запроса и ответа на него
+public class DummyServer {
+    public static void startServer() throws IOException {
+        //1-ый параметр - порт, на котором будет запущен сервер, 2-ой - лог
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        HttpHandler getServerStatus = DummyServer::getServerStatus;
+        //метод для приема запросов, 1-ый параметр - url, который будет слушаться, 2-ой - метод обработки запроса и отправки ответа
+        server.createContext("/", getServerStatus);
+    }
+
+    private static void getServerStatus(HttpExchange exchange) throws IOException { //объект, который будет передаваться в вызов метода Java-Web-сервером
+        String responseText = "Bot is running"; //текст ответа
+        exchange.sendResponseHeaders(200, responseText.getBytes().length); //отправка заголовка ответа
+        //1-ый параметр - код ответа, 2-ой - длина ответа
+        exchange.getResponseBody().write(responseText.getBytes()); //запись текста ответа
+        exchange.close(); //закрытие объекта
+    }
+}
